befaf4df4b4a5ac29b339d6b7b2b4b6160751a9e
invest7777 <mendocanunes187@gmail.com>
2026-02-25 01:03:41 -0300

    Update bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,35 +1,25 @@
+import os
 import telebot
 import yfinance as yf
-import time
 
-# --- CONFIGURAÇÕES ---
-TOKEN = "7953321564:AAHd_tbtINcXmG31yHF5W7AnY4fnypQLqsQ"
-CHAT_ID = "1430932470"
+# Busca as chaves que você salvou no GitHub Secrets
+TOKEN = os.getenv('TELEGRAM_TOKEN')
+CHAT_ID = os.getenv('CHAT_ID')
+
 bot = telebot.TeleBot(TOKEN)
 
-# Sua carteira de R$ 31.773,28
-carteira = {
-    "MXRF11.SA": 2001.24, "RECR11.SA": 2090.66, "VGHF11.SA": 3009.60, 
-    "VISC11.SA": 2097.03, "XPML11.SA": 3448.44, "BTCI11.SA": 2008.10, 
-    "HGLG11.SA": 5037.76, "KNCR11.SA": 10080.45
-}
-
-def enviar_resumo():
-    total_patrimonio = 0
-    mensagem = "🏦 *RELATÓRIO DIÁRIO - CARTEIRA FII*\n\n"
-    
-    for ticker, valor in carteira.items():
-        try:
-            fundo = yf.Ticker(ticker)
-            time.sleep(1) 
-            total_patrimonio += valor
-            mensagem += f"🔹 *{ticker.replace('.SA', '')}*: R$ {valor:,.2f}\n"
-        except:
-            continue
-    
-    mensagem += f"\n💰 *Total Investido: R$ {total_patrimonio:,.2f}*"
-    bot.send_message(CHAT_ID, mensagem, parse_mode="Markdown")
+def enviar_cotacao():
+    try:
+        # Busca cotação do Dólar (USDBRL=X)
+        data = yf.Ticker("USDBRL=X")
+        preco = data.history(period="1d")['Close'].iloc[-1]
+        
+        mensagem = f"🤖 *Relatório Diário*\n\n💵 Cotação do Dólar: R$ {preco:.2f}"
+        
+        bot.send_message(CHAT_ID, mensagem, parse_mode="Markdown")
+        print("Mensagem enviada com sucesso!")
+    except Exception as e:
+        print(f"Erro ao buscar dados: {e}")
 
 if __name__ == "__main__":
-    enviar_resumo()
-
+    enviar_cotacao()
